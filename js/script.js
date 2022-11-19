9bd990ca9e920328e39ac15fc1c857ab22e14e04
kubajot90 <kubajot90@gmail.com>
2022-11-19 20:57:30 +0100

    Add  hamburger icon and hero logo.

--- js/script.js
+++ js/script.js
@@ -2,8 +2,7 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-    //   delay: 5500,
-      delay: 55222200,
+      delay: 5500,
       disableOnInteraction: false,
     },
     pagination: {
