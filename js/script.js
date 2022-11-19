bb4462610c3d92d71598034e93003448b3963e55
kubajot90 <kubajot90@gmail.com>
2022-11-19 17:18:56 +0100

    Style pagination of swiper

--- js/script.js
+++ js/script.js
@@ -2,15 +2,13 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 2500,
+    //   delay: 5500,
+      delay: 55222200,
       disableOnInteraction: false,
     },
     pagination: {
       el: ".swiper-pagination",
       clickable: true,
     },
-    navigation: {
-      nextEl: ".swiper-button-next",
-      prevEl: ".swiper-button-prev",
-    },
+   
   });
