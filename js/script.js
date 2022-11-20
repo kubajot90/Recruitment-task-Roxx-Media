9dffbc298024534c1cd3e8148dc1c89630d17b94
kubajot90 <kubajot90@gmail.com>
2022-11-20 08:59:42 +0100

    Add srcset for retina displays

--- js/script.js
+++ js/script.js
@@ -2,7 +2,8 @@ var swiper = new Swiper(".mySwiper", {
     spaceBetween: 30,
     centeredSlides: true,
     autoplay: {
-      delay: 5500,
+      // delay: 5500,
+      delay: 552200,
       disableOnInteraction: false,
     },
     pagination: {
